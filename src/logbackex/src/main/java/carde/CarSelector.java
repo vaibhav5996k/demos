af018606124a9da469504926d8da09f32e697253
vaibhav <vibhukatkar96@gmail.com>
2019-09-07 16:13:56 +0530

    ex

--- src/logbackex/src/main/java/carde/CarSelector.java
+++ src/logbackex/src/main/java/carde/CarSelector.java
@@ -2,15 +2,27 @@ package carde;
 import java.io.IOException;
 
 public class CarSelector {
-    public static void main(String[] arguments)  {
+    public static void main(String[] arguments) {
         CarServices carService = new CarServices();
         for (String argument : arguments) {
-            try {
+            if (isValid(argument)) {
                 carService.process(argument);
-            }catch (RuntimeException e){
-                 System.out.println(e.getMessage());
+            } else{
+                System.err.println("Ignoring invalid argument:"+argument);
             }
+        }
+    }
+    private static boolean isValid(String argument) {
+        try {
+            CarState carstate = CarState.valueOf(argument);
+            System.out.print("somtttt");
+            System.out.print("erfgtdb");
+            System.out.print("gwrtg");
+            carstate = null;
+        }catch (IllegalArgumentException e){
+
 
         }
+        return false;
     }
 }
